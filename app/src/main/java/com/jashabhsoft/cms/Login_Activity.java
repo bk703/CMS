42695fd218778b1a02588e9c68067ba6a9b4a3fe
abubakarhussain <44763504+abubakarhussain@users.noreply.github.com>
2020-09-08 22:17:34 +0500

    app icon changed

--- app/src/main/java/com/jashabhsoft/cms/Login_Activity.java
+++ app/src/main/java/com/jashabhsoft/cms/Login_Activity.java
@@ -10,5 +10,6 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
     }
 }
