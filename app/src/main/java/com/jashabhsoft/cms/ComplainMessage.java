d1a94a301cb483aef9d6e89d5258f02c712f7c68
ahmad <m.ahmad1ebcse292@gmail.com>
2020-09-10 15:55:39 +0500

    Added edit profile and complain message layout

---
+++ app/src/main/java/com/jashabhsoft/cms/ComplainMessage.java
@@ -0,0 +1,14 @@
+package com.jashabhsoft.cms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ComplainMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_message);
+    }
+}
