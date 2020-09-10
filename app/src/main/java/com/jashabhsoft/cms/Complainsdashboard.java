a33d4013172c2b34adb41664b70cd582f99bc63d
ahmad <m.ahmad1ebcse292@gmail.com>
2020-09-10 16:46:43 +0500

    Added Complainsdashboard

--- app/src/main/java/com/jashabhsoft/cms/Complainsdashboard.java
+++ app/src/main/java/com/jashabhsoft/cms/Complainsdashboard.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ComplainMessage extends AppCompatActivity {
+public class Complainsdashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_complain_message);
+        setContentView(R.layout.activity_complainsdashboard);
     }
 }
